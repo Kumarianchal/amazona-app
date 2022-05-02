385f0b39a6ff35481133270e4f175a236c1d5905
Anchal <anchalkumaria39it@gmail.com>
2022-05-02 21:03:57 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Himachal Apples',
+            category: 'Fruits',
+            image: '/images/p1.jpg',
+            price:120,
+            brand:'Lajawab',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Guava',
+            category: 'Fruits',
+            image: '/images/p1.jpg',
+            price:100,
+            brand:'Lajawab',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Brinjal',
+            category: 'Vegetable',
+            image: '/images/p1.jpg',
+            price:40,
+            brand:'Lajawab',
+            rating:5,
+            numReviews:20,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Mango',
+            category: 'Fruits',
+            image: '/images/p1.jpg',
+            price:120,
+            brand:'Alphanso',
+            rating:5,
+            numReviews:20,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Potato',
+            category: 'Vegetables',
+            image: '/images/p1.jpg',
+            price:20,
+            brand:'Lajawab',
+            rating:4.5,
+            numReviews:10,
+            description:'good quality product'
+        },
+        {
+            _id:'6',
+            name:'Orange',
+            category: 'Fruits',
+            image: '/images/p1.jpg',
+            price:90,
+            brand:'Nagpuria',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+    ],
+};
+export default data;
